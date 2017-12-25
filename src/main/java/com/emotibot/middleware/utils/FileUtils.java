8ef2e3976800579d1a2943aa8a12875f7172680a
yanbinwang <yanbinwang@office.emotibot.com>
2017-12-25 11:23:35 +0800

    添加文件读写Utils

---
+++ src/main/java/com/emotibot/middleware/utils/FileUtils.java
@@ -0,0 +1,98 @@
+package com.emotibot.middleware.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class FileUtils
+{
+    public static List<String> readFile(String fileName)
+    {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        List<String> ret = new ArrayList<String>();
+        try 
+        {
+            reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) 
+            {
+                ret.add(line);
+            }
+            return ret;
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+            return null;
+        } 
+        finally 
+        {
+            if (reader != null) 
+            {
+                try 
+                {
+                    reader.close();
+                } 
+                catch (IOException e1) 
+                {
+                    
+                }
+            }
+        }
+    }
+    
+    public static void writeLogForXls(String xlsFile, List<List<String>> contents)
+    {
+        File file = new File(xlsFile);
+        if (file.exists())
+        {
+            file.delete();
+        }
+        OutputStream os = null;
+        XSSFWorkbook xssfWorkbook = null;
+        try
+        {
+            os = new FileOutputStream(xlsFile);
+            xssfWorkbook = new XSSFWorkbook();
+            Sheet sheet = xssfWorkbook.createSheet();
+            int rowCount = 0;
+            for (List<String> content : contents)
+            {
+                Row row = sheet.createRow(rowCount);
+                for (int i = 0; i < content.size(); i ++)
+                {
+                    Cell cell = row.createCell(i);
+                    cell.setCellValue(content.get(i));
+                }
+                rowCount ++;
+            }
+            xssfWorkbook.write(os);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                os.close();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}
