12be2cb5059311902e96981e99fbeeab67a43275
yanbinwang <yanbinwang@office.emotibot.com>
2018-02-05 09:28:38 +0800

    修改FileUtils

--- src/main/java/com/emotibot/middleware/utils/FileUtils.java
+++ src/main/java/com/emotibot/middleware/utils/FileUtils.java
@@ -1,11 +1,14 @@
 package com.emotibot.middleware.utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,42 +34,6 @@ public class FileUtils
                 ret.add(line);
             }
             return ret;
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-            return null;
-        } 
-        finally 
-        {
-            if (reader != null) 
-            {
-                try 
-                {
-                    reader.close();
-                } 
-                catch (IOException e1) 
-                {
-                    
-                }
-            }
-        }
-    }
-    
-    public static String readFileToString(String fileName)
-    {
-        File file = new File(fileName);
-        BufferedReader reader = null;
-        StringBuilder sb = new StringBuilder();
-        try 
-        {
-            reader = new BufferedReader(new FileReader(file));
-            String line = null;
-            while ((line = reader.readLine()) != null) 
-            {
-                sb.append(line);
-            }
-            return sb.toString();
         } 
         catch (IOException e) 
         {
@@ -158,6 +125,72 @@ public class FileUtils
             catch (Exception e)
             {
                 
+            }
+        }
+    }
+    
+    public static void appendFiles(List<String> filePaths, String targetFile)
+    {
+        FileWriter fw = null;
+        try
+        {
+            fw = new FileWriter(targetFile);
+            for (String filePath : filePaths)
+            {
+                List<String> lines = readFile(filePath);
+                for (int i = 0; i < lines.size(); i ++)
+                {
+                    fw.write(lines.get(i) + "\r\n");
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return;
+        }
+        finally
+        {
+            try
+            {
+                fw.close();
+            } 
+            catch (IOException e)
+            {
+                
+            }
+        }
+    }
+    
+    public static String readFileToString(String file)
+    {
+        InputStream in = null;
+        try
+        {
+            in = new FileInputStream(file);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024 * 4];
+            int n = 0;
+            while ((n = in.read(buffer)) != -1) 
+            {
+                out.write(buffer, 0, n);
+            }
+            return new String(out.toByteArray());
+        } 
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            try
+            {
+                in.close();
+            } 
+            catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
     }
