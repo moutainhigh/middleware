fabefa16d3887c5293b9876d67447711ae419991
yanbinwang <yanbinwang@office.emotibot.com>
2018-03-14 10:31:19 +0800

    增加了FileUtils的功能

--- src/main/java/com/emotibot/middleware/utils/FileUtils.java
+++ src/main/java/com/emotibot/middleware/utils/FileUtils.java
@@ -10,13 +10,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class FileUtils
 {
@@ -81,6 +87,138 @@ public class FileUtils
             catch (IOException e)
             {
                 
+            }
+        }
+    }
+    
+    public static List<List<String>> readFileFromCsv(String csvFile)
+    {
+        File file = new File(csvFile);
+        FileReader fReader = null;
+        CSVReader csvReader = null; 
+        try
+        {
+            fReader = new FileReader(file);
+            csvReader = new CSVReader(fReader);
+            List<String[]> list = csvReader.readAll();
+            List<List<String>> ret = new ArrayList<List<String>>();
+            for (String[] ss : list)
+            {
+                List<String> csvList = new ArrayList<String>();
+                for (String str : ss)
+                {
+                    csvList.add(str);
+                }
+                ret.add(csvList);
+            }
+            return ret;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            try
+            {
+                csvReader.close();
+            } 
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void writeFileToCsv(String csvFile, List<List<String>> contexts)
+    {
+        File file = new File(csvFile);
+        FileWriter writer = null; 
+        CSVWriter csvWriter = null;
+        try
+        {
+            writer = new FileWriter(file);
+            csvWriter = new CSVWriter(writer, ','); 
+            List<String[]> csvContexts = new ArrayList<String[]>();
+            for (List<String> context : contexts)
+            {
+                if (context.isEmpty())
+                {
+                    continue;
+                }
+                int size = context.size();
+                String[] csvContext = new String[size];
+                for (int i = 0; i < size; i ++)
+                {
+                    csvContext[i] = context.get(i);
+                }
+                csvContexts.add(csvContext);
+            }
+            csvWriter.writeAll(csvContexts);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                csvWriter.close();
+            } 
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static List<List<String>> readFileFromXlsx(String xlsxFile)
+    {
+        InputStream is = null;
+        XSSFWorkbook xssfWorkbook = null;
+        try
+        {
+            is = new FileInputStream(xlsxFile);
+            xssfWorkbook = new XSSFWorkbook(is);
+            XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+            if (xssfSheet == null)
+            {
+                return null;
+            }
+            List<List<String>> ret = new ArrayList<List<String>>();
+            for (int i = 0; i <= xssfSheet.getLastRowNum(); i ++)
+            {
+                XSSFRow xssfRow = xssfSheet.getRow(i);
+                if (xssfRow != null)
+                {
+                    List<String> elements = new ArrayList<String>();
+                    for (int j = 0; j < xssfRow.getLastCellNum(); j ++)
+                    {
+                        elements.add(xssfRow.getCell(j).getStringCellValue());
+                    }
+                    ret.add(elements);
+                }
+            }
+            return ret;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+        finally
+        {
+            try
+            {
+                if (is != null)
+                {
+                    is.close();
+                }
+            } 
+            catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
     }
